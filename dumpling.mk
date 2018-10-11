5f4f9af3670ce2cfa9fdb2c000140e58267d8d84
Mateusz Korzeniewski <themrp8@gmail.com>
2018-10-11 22:10:56 +0200

    Create dumpling.mk

---
+++ dumpling.mk
@@ -0,0 +1,14 @@
+<?xml version="1.0" coding="UTF-8"?>
+
+<manifest>
+
+    <!-- local manifest for XenonHD-microG Pie for OnePlus 5T -->
+
+    <project name="MattRootCM/android_device_oneplus_dumpling" path="device/oneplus/dumpling" revision="xe9" remote="github" />
+    <project name="LineageOS/android_kernel_oneplus_msm8998" path="kernel/oneplus/msm8998" revision="lineage-16.0" remote="github" />
+    <project name="LineageOS/android_device_oneplus_msm8998-common" path="device/oneplus/msm8998-common" revision="lineage-16.0" remote="github" />
+    <project name="LineageOS/android_device_oppo_common" path="device/oppo/common" revision="lineage-16.0" remote="github" />
+    <project name="TheMuppets/proprietary_vendor_oneplus" path="vendor/oneplus" revision="lineage-16.0" remote="github" />
+    <project name="android_prebuilts_prebuiltapks/android_prebuilts_prebuiltapks" path="prebuilts/prebuiltapks" revision="master" remote="github" />
+
+</manifest>
