c949445f45797f25d541b1a4c78c1642591d0461
Mateusz Korzeniewski <themrp8@gmail.com>
2018-10-11 22:15:25 +0200

    what the shit did i write

--- dumpling.mk
+++ dumpling.mk
@@ -9,6 +9,6 @@
     <project name="LineageOS/android_device_oneplus_msm8998-common" path="device/oneplus/msm8998-common" revision="lineage-16.0" remote="github" />
     <project name="LineageOS/android_device_oppo_common" path="device/oppo/common" revision="lineage-16.0" remote="github" />
     <project name="TheMuppets/proprietary_vendor_oneplus" path="vendor/oneplus" revision="lineage-16.0" remote="github" />
-    <project name="android_prebuilts_prebuiltapks/android_prebuilts_prebuiltapks" path="prebuilts/prebuiltapks" revision="master" remote="github" />
+    <project name="lineageos4microg/android_prebuilts_prebuiltapks" path="prebuilts/prebuiltapks" revision="master" remote="github" />
 
 </manifest>
